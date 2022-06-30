2f68659deed76a60f6eb9a1273ae114e6d7df009
pikjung <tanjungfikri1@gmail.com>
2022-06-30 19:30:21 +0700

    Penambahan method index dan show bagian admin SS

--- app/Http/Controllers/admin/adminSSAdminController.php
+++ app/Http/Controllers/admin/adminSSAdminController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\User;
 
 class adminSSAdminController extends Controller
 {
     //
+    public function index()
+    {
+        $user = User::where('role_id',3)->get();
+        return view('admin_kas.admin_ss.admin_ss', ['users' => $user]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json(['user' => $user], 200);
+    }
 }
