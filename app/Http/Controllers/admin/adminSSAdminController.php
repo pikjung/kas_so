72685df4829d063c9185bec3cc03c7ea9bd2a40b
pikjung <tanjungfikri1@gmail.com>
2022-06-30 19:32:54 +0700

    Penambahan method tambah data untuk adminSS

--- app/Http/Controllers/admin/adminSSAdminController.php
+++ app/Http/Controllers/admin/adminSSAdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class adminSSAdminController extends Controller
 {
@@ -19,5 +20,23 @@ class adminSSAdminController extends Controller
     {
         $user = User::findOrFail($id);
         return response()->json(['user' => $user], 200);
+    }
+
+    public function tambah(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        User::create([
+            'role_id' => 3,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->back()->with(['success' => true, 'message' => 'Admin SS successfully Created']);
     }
 }
