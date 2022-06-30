d49fdbc75cfb5bd5be63bbe44580d7703977b298
pikjung <tanjungfikri1@gmail.com>
2022-06-30 22:04:22 +0700

    Penambahan view admin_ss dan sales

---
+++ resources/views/admin_kas/admin_ss/admin_ss.blade.php
@@ -0,0 +1,44 @@
+@extends('admin_kas.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="mt-5">
+
+        </div>
+
+        <div class="card mt-5">
+            <div class="card-header">
+                Admin SS
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>Username</th>
+                            <th>Aksi</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>{{$user->username}}</td>
+                                    <td>
+                                        <button class="btn btn-warning btn-sm">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button class="btn btn-danger btn-sm">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
