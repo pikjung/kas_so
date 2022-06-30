fd511784fac686fc697100db0825617cbdb17f89
pikjung <tanjungfikri1@gmail.com>
2022-06-30 16:26:40 +0700

    Penambahan controller admin untuk modul sales

---
+++ app/Http/Controllers/admin/adminSalesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Models\sales;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class adminSalesController extends Controller
+{
+    //
+    public function index()
+    {
+        $sales = sales::orderBy('created_at', 'desc')->get();
+        return view('admin_kas.sales.sales', ['sales' => $sales]);
+    }
+}
