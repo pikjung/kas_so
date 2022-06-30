e98fb54e0167d2b7bc4dfb33dc65c7f15efabc5c
pikjung <tanjungfikri1@gmail.com>
2022-06-30 16:34:31 +0700

    Penambahan method store sales data

--- app/Http/Controllers/admin/adminSalesController.php
+++ app/Http/Controllers/admin/adminSalesController.php
@@ -2,22 +2,52 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\area;
+use App\Models\User;
 use App\Models\sales;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class adminSalesController extends Controller
 {
     //
     public function index()
     {
+        $area = area::all();
         $sales = sales::orderBy('created_at', 'desc')->get();
-        return view('admin_kas.sales.sales', ['sales' => $sales]);
+        return view('admin_kas.sales.sales', ['sales' => $sales, 'areas' => $area]);
     }
 
     public function show($id)
     {
         $sales = sales::findOrFail($id);
         return response()->json(['sales' => $sales], 200);
+    }
+
+    public function tambah(Request $request)
+    {
+        $request->validate([
+            'area_id' => 'required',
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'role_id' => '4',
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        sales::create([
+            'user_id' => $user->id,
+            'area_id' => $request->area_id,
+        ]);
+
+        return redirect()->back()->with(['success' => true, 'message' => 'Sales successfully created']);
     }
 }
